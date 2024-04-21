b37a6ef3f7e0498941e86591e17a1cdd1879604e
ArtemKarlash <154004470+ArtemKarlash@users.noreply.github.com>
2024-04-21 11:54:21 +0300

    Vine

---
+++ js/main.js
@@ -0,0 +1,15 @@
+    // var my_name = "Artem";
+    // let lastName = "Karlash";
+    // const work = "developer";
+    // let my_age = 11;
+
+    // let about_me = `My name is ${my_name}. I'm will be ${IT}.`
+
+    // console.log(my_name.length) 
+    // console.log(about_me)
+
+
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+    });
